1c2099a4fa0abcd638e8abe280136ae922bb27b0
Krishna <gkrrish.11@gmail.com>
2024-08-03 16:25:18 +0530

    WIP :: still need to fix this

--- src/main/java/com/now/tele/external/InitalRequestService.java
+++ src/main/java/com/now/tele/external/InitalRequestService.java
@@ -8,6 +8,10 @@ import java.util.Base64;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -29,68 +33,78 @@ import com.now.tele.util.TelegramMessageUtility;
 @Service
 public class InitalRequestService {
 
-    @Autowired
-    private RestTemplate restTemplate;
+	@Autowired
+	private RestTemplate restTemplate;
 
-    @Autowired
-    private MyTelegramBot telegramBot;
+	@Autowired
+	private MyTelegramBot telegramBot;
 
-    private static final String BASE_URL = "http://localhost:9009/welcome";
+	private static final String BASE_URL = "http://localhost:9009/welcome";
 
 	public void handleInitialRequest(String messageText, long chatId) throws IOException, TelegramApiException {
 		WelcomeRequest welcomeRequest = new WelcomeRequest();
 		welcomeRequest.setMobileNumber(Long.toString(chatId));
 		welcomeRequest.setWelcomeAnyMessage(messageText);
 
-		ResponseEntity<WelcomeResponse> response = restTemplate.postForEntity(BASE_URL, welcomeRequest, WelcomeResponse.class);
-		WelcomeResponse welcomeResponse = response.getBody();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", "application/json, application/pdf, image/png");
 
-		if (welcomeResponse instanceof WelcomeBackPDFResponse) {
-			WelcomeBackPDFResponse pdfResponse = (WelcomeBackPDFResponse) welcomeResponse;
-			byte[] pdfData = Base64.getDecoder().decode(pdfResponse.getInvoiceBase64());
-			String message = pdfResponse.getMessage();
-			String delta   = pdfResponse.getDelta();
+		HttpEntity<WelcomeRequest> httpEntity = new HttpEntity<>(welcomeRequest, headers);
+
+		ResponseEntity<Resource> response = restTemplate.postForEntity(BASE_URL, httpEntity, Resource.class);
+		String contentType = response.getHeaders().getContentType().toString();
+
+		if (contentType.equals("application/pdf")) {
+			byte[] pdfData = response.getBody().getInputStream().readAllBytes();
 			
-			sendPdfResponse(chatId, pdfData, message, delta);
+			sendPdfResponse(chatId, pdfData, "Here is your requested PDF", "Do you want to change?");
 			
-		} else if (welcomeResponse instanceof WelcomeImageResponse) {
-			WelcomeImageResponse imageResponse = (WelcomeImageResponse) welcomeResponse;
-			byte[] imageData = Base64.getDecoder().decode(imageResponse.getImageBase64());
-			String message = imageResponse.getMessage();
+		} else if (contentType.equals("image/png")) {
+			byte[] imageData = response.getBody().getInputStream().readAllBytes();
 			
-			List<String> languages = imageResponse.getLanguages();
+			sendImageResponse(chatId, imageData, "Here is your requested image", List.of("Language 1", "Language 2"));
 			
-			sendImageResponse(chatId, imageData, message , languages);
+		} else if (contentType.equals("application/json")) {
+			WelcomeResponse welcomeResponse = restTemplate.getForObject(BASE_URL, WelcomeResponse.class);
+			if (welcomeResponse instanceof WelcomeBackPDFResponse) {
+				WelcomeBackPDFResponse pdfResponse = (WelcomeBackPDFResponse) welcomeResponse;
+				byte[] pdfData = Base64.getDecoder().decode(pdfResponse.getInvoiceBase64());
+				sendPdfResponse(chatId, pdfData, pdfResponse.getMessage(), pdfResponse.getDelta());
+				
+			} else if (welcomeResponse instanceof WelcomeImageResponse) {
+				WelcomeImageResponse imageResponse = (WelcomeImageResponse) welcomeResponse;
+				byte[] imageData = Base64.getDecoder().decode(imageResponse.getImageBase64());
+				sendImageResponse(chatId, imageData, imageResponse.getMessage(), imageResponse.getLanguages());
+				
+			} else {
+				sendTextResponse(chatId, welcomeResponse.getMessage());
+			}
 		} else {
-			String message = welcomeResponse.getMessage();
-			sendTextResponse(chatId, message);
+			sendTextResponse(chatId, "Unsupported response type");
 		}
 	}
 
-    private void sendPdfResponse(long chatId, byte[] pdfData, String message, String delta) throws TelegramApiException {
-    	String todayDate = LocalDate.now().format(DateTimeFormatter.ofPattern("MMMYYYY"));
-    	
-    	InlineKeyboardMarkup markup = ButtonUtility.createInlineKeyboard(List.of(delta)); 
-    	
-        InputFile inputFile = new InputFile(new ByteArrayInputStream(pdfData), todayDate + "_invoice.pdf");
-        SendDocument documentMessage = TelegramMessageUtility.createDocumentMessage(chatId, inputFile, message, markup);
-        telegramBot.execute(documentMessage);
-    }
+	private void sendPdfResponse(long chatId, byte[] pdfData, String message, String delta) throws TelegramApiException {
+		String todayDate = LocalDate.now().format(DateTimeFormatter.ofPattern("MMMYYYY"));
+		InlineKeyboardMarkup markup = ButtonUtility.createInlineKeyboard(List.of(delta));
 
-    private void sendImageResponse(long chatId, byte[] imageData, String message, List<String> languages) throws TelegramApiException {
-        InlineKeyboardMarkup markup = ButtonUtility.createInlineKeyboard(languages); 
+		InputFile inputFile = new InputFile(new ByteArrayInputStream(pdfData), todayDate + "_invoice.pdf");
+		SendDocument documentMessage = TelegramMessageUtility.createDocumentMessage(chatId, inputFile, message, markup);
+		telegramBot.execute(documentMessage);
+	}
 
-        // Create an InputFile from the byte array
-        InputFile inputFile = new InputFile(new ByteArrayInputStream(imageData), "welcome-banner.png");
+	private void sendImageResponse(long chatId, byte[] imageData, String message, List<String> languages) throws TelegramApiException {
+		InlineKeyboardMarkup markup = ButtonUtility.createInlineKeyboard(languages);
+		
+		InputFile inputFile = new InputFile(new ByteArrayInputStream(imageData), "welcome-banner.png");
+		SendPhoto photoMessage = TelegramMessageUtility.createPhotoMessage(chatId, inputFile, message, markup);
+		telegramBot.execute(photoMessage);
+	}
 
-        // Use the InputFile in the SendPhoto method
-        SendPhoto photoMessage = TelegramMessageUtility.createPhotoMessage(chatId, inputFile, message, markup);
-        telegramBot.execute(photoMessage);
-    }
-    
-    private void sendTextResponse(long chatId, String messageText) throws TelegramApiException {
-        InlineKeyboardMarkup markup = ButtonUtility.createInlineKeyboard(List.of("Option 1", "Option 2")); // Example buttons
-        SendMessage textMessage = TelegramMessageUtility.createTextMessage(chatId, messageText, markup);
-        telegramBot.execute(textMessage);
-    }
+	private void sendTextResponse(long chatId, String messageText) throws TelegramApiException {
+		InlineKeyboardMarkup markup = ButtonUtility.createInlineKeyboard(List.of("Option 1", "Option 2")); 
+		
+		SendMessage textMessage = TelegramMessageUtility.createTextMessage(chatId, messageText, markup);
+		telegramBot.execute(textMessage);
+	}
 }
